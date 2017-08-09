53507b421c4c82eb98d77c30425983543e740504
Richard Buckle <richardb@sailmaker.co.uk>
2017-08-09 21:44:47 +0100

    Arm purgeConfigGraphics.sh

--- purgeConfigGraphics.sh
+++ purgeConfigGraphics.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # purge all .jpg files over 1 day old
-find /home/edrefcardinfoi7/www/configs -name "*.jpg" \! \( -newerct '1 day ago' \) -print
+find /home/edrefcardinfoi7/www/configs -name "*.jpg" \! \( -newerct '1 day ago' \) -delete
