16028ffdd65261d492946a1b75809dc2cf9908d3
Richard Buckle <richardb@sailmaker.co.uk>
2017-08-14 22:34:37 +0100

    provide a default description for untitled configs

--- www/scripts/bindings.py
+++ www/scripts/bindings.py
@@ -2928,6 +2928,8 @@ def main():
     
     # Save variables for later replays
     if mode is Mode.generate:
+        if description == '':
+            description = 'untitled %s config' % devices
         saveReplayInfo(config, description, styling, displayGroups, devices, showKeyboard, errors)
     
     printHTML(mode, config, public, createdImages, deviceForBlockImage, errors)
