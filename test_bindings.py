2c138b5c2a3988a667efa8e8af36daf7715ab5ce
Richard Buckle <richardb@sailmaker.co.uk>
2017-08-23 16:36:24 +0100

    Add test_bindings.py
    Initially just one failing test

---
+++ test_bindings.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import unittest
+
+
+class BindingsTests(unittest.TestCase):
+
+    def testOne(self):
+        self.fail()
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
