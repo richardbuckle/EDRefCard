6c6163904293498a3497649610b2913f7f3edc3b
Richard Buckle <richardb@sailmaker.co.uk>
2017-08-26 14:02:57 +0100

    Absolute not relative dir

--- purgeConfigGraphics.sh
+++ purgeConfigGraphics.sh
@@ -2,4 +2,4 @@
 DIR=`dirname "$0"`
 
 # purge all .jpg files over 1 day old
-find "./$DIR/www/configs" -name "*.jpg" \! \( -newerct '1 day ago' \) -delete
+find "$DIR/www/configs" -name "*.jpg" \! \( -newerct '1 day ago' \) -delete
