6e484a56cb07a55441417de3e551e042091ffb9e
Richard Buckle <richardb@sailmaker.co.uk>
2017-08-27 22:37:08 +0100

    #4 Now correctly identifying files to purge

--- purgePrivateConfigs.py
+++ purgePrivateConfigs.py
@@ -2,7 +2,7 @@
 
 '''
 Walk the configs dir and reap all unpublished configs.
-These are configs that have no saved pickle file.
+These are configs that have no saved ".replay" pickle file.
 '''
 
 import sys
@@ -12,14 +12,37 @@ from pathlib import Path
 class Purger:
 
     def __init__(self):
-        self.configsDir = Path('./www/configs/')
+        self.configsDir = Path('./www/configs')
     
+    def allBindings(self):
+        return list(self.configsDir.glob('**/*.binds'))
+    
+    def replayPath(self, path):
+        return path.with_suffix('.replay')
+    
+    def hasReplay(self, bindPath):
+        replayPath = self.replayPath(bindPath)
+        return replayPath.exists()
         
+    def thoseWithoutReplay(self, bindingsPaths):
+        return [path for path in bindingsPaths if not self.hasReplay(path)]    
+    
+    def allFilesWithStartingWithStem(self, path):
+        nameGlob = '%s*.*' % path.stem
+        parent = path.parent
+        return list(parent.glob(nameGlob))
+    
     def purge(self):
         if not self.configsDir.exists():
-            # script probably installed in the wrong place or called with the wrong cwd
+            # script probably installed in the wrong place or called with the wrong working directory
             sys.exit('%s not found' % self.configsDir)
-
+        allBindings = self.allBindings()
+        privateBindings = self.thoseWithoutReplay(allBindings)
+        print([path.stem for path in privateBindings])
+        x= privateBindings[0]
+        print(x)
+        print( self.allFilesWithStartingWithStem(x) )
+        
 
 def main():
     purger = Purger()
