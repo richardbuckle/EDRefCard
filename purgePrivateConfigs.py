ca69d78cef920b81202faeb760d5a92a5cf8708c
Richard Buckle <richardb@sailmaker.co.uk>
2017-08-27 21:24:05 +0100

    Purger will exit if it cannot find configsDir

--- purgePrivateConfigs.py
+++ purgePrivateConfigs.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python3
 
-def main():   # pragma: no cover
-    pass
+'''
+Walk the configs dir and reap all unpublished configs.
+These are configs that have no saved pickle file.
+'''
 
-if __name__ == '__main__':   # pragma: no cover
+import sys
+from pathlib import Path
+
+
+class Purger:
+
+    def __init__(self):
+        self.configsDir = Path('./www/configs/')
+    
+        
+    def purge(self):
+        if not self.configsDir.exists():
+            # script probably installed in the wrong place or called with the wrong cwd
+            sys.exit('%s not found' % self.configsDir)
+
+
+def main():
+    purger = Purger()
+    purger.purge()
+
+if __name__ == '__main__':
     main()
