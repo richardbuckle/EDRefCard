23b854f05e9915380579567a321a66868968703b
Richard Buckle <richardb@sailmaker.co.uk>
2019-05-07 01:03:07 +0100

    test

--- www/scripts/bindings.py
+++ www/scripts/bindings.py
@@ -714,7 +714,7 @@ def printListItem(configObj):
 
 def printDeviceList():
     print('<div id="banner"><h1>EDRefCard: supported devices</h1></div>')
-    devices = supportedDevices.keys().sort()
+    devices = supportedDevices.keys()
     print(devices)
 
 def printList():
